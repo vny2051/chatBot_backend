f89cca6db6c972e83341a75d48eb54d1f0d275c9
vny2051 <75247375+vny2051@users.noreply.github.com>
2024-06-15 03:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask, render_template
+from flask import jsonify, request
+import nltk
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+from keras.models import load_model
+import json
+import random
+import pickle
+
+from flask_cors import CORS
 
 app = Flask(__name__)
 
