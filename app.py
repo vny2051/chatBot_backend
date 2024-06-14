a3cc9c10572381c48fd7eb70c182aa8b7cbabb4e
vny2051 <75247375+vny2051@users.noreply.github.com>
2024-06-15 03:18:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import nltk
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+from keras.models import load_model
+import json
+import random
+import pickle
 
 app = Flask(__name__)
 
